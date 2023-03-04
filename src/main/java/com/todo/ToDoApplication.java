d087cf3c479df6c96e80c5561be764961f2e5442
rajasekhar1404 <mrrajasekhar09@gmail.com>
2023-03-04 23:37:27 +0530

    to check emails from jenkins

--- src/main/java/com/todo/ToDoApplication.java
+++ src/main/java/com/todo/ToDoApplication.java
@@ -1,25 +1,13 @@
 package com.todo;
 
-import jakarta.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToDoApplication {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(ToDoApplication.class);
-
-    @PostConstruct
-    public void init(){
-        LOGGER.info("Application started...");
-    }
-
     public static void main(String[] args) {
-        LOGGER.info("Application executing...");
         SpringApplication.run(ToDoApplication.class, args);
-        LOGGER.info("Application executed...");
     }
 
 }
